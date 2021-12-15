93adf315cdb312bfccb002f378c14bff1c0e7680
qtt153759 <qtt153759@gmail.com>
2021-12-15 22:42:47 +0700

    Hoàn Thiện Chứa Năng Bổ Sung Thông Tin Chi Tiết Cho Bác Sỹ

--- src/controller/doctorController.js
+++ src/controller/doctorController.js
@@ -12,7 +12,30 @@ let getTopDoctorHome = async (req, res) => {
         });
     }
 };
-
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (e) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "error from the server..",
+        });
+    }
+};
+let postInforDoctor = async (req, res) => {
+    try {
+        let response = await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (e) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "error from the server..",
+        });
+    }
+};
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    postInforDoctor: postInforDoctor,
 };
