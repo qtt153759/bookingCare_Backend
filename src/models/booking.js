52b98242a3c05952ffa6436eda58dbf33b4b994d
qtt153759 <qtt153759@gmail.com>
2022-01-03 18:17:15 +0700

    Viết api tạo schedule booking phía patient

--- src/models/booking.js
+++ src/models/booking.js
@@ -1,29 +1,30 @@
-'use strict';
+"use strict";
 // duoc khoi tao model san bang cau lenh nay :npx sequelize-cli model:generate --name Booking --attributes firstName:string,lastName:string,email:string
-const {
-  Model
-} = require('sequelize');
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Booking extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Booking extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  };
-  Booking.init({
-    // khong can khai bao id:DataTypes.INTEGER,
-    statusId:DataTypes.STRING,
-    doctorId: DataTypes.INTEGER,//doctorId la id cua bang user -> la integer
-    patientid: DataTypes.INTEGER,
-    date: DataTypes.DATE,
-    timeType: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Booking',
-  });
-  return Booking;
+    Booking.init(
+        {
+            // khong can khai bao id:DataTypes.INTEGER,
+            statusId: DataTypes.STRING,
+            doctorId: DataTypes.INTEGER, //doctorId la id cua bang user -> la integer
+            patientId: DataTypes.INTEGER,
+            date: DataTypes.STRING, //date mà dùng kiểu date thì lúc query lên rất là mệt => dùng String
+            timeType: DataTypes.STRING,
+        },
+        {
+            sequelize,
+            modelName: "Booking",
+        }
+    );
+    return Booking;
 };
---
+++ src/service/patientService.js
@@ -0,0 +1,45 @@
+import db from "../models/index";
+
+require("dotenv").config();
+let postBookAppointment = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.email || !data.doctorId || !data.timeType || !data.date) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required parameter",
+                });
+            } else {
+                let user = await db.User.findOrCreate({
+                    where: { email: data.email },
+                    default: {
+                        email: data.email,
+                        roleId: "R3",
+                    },
+                });
+                if (user && user[0]) {
+                    //hàm findOrCreate sẽ trả ra 1 mảng trong đó user[0] là data, user[1] là true/false tùy vào update hay create
+                    await db.Booking.findOrCreate({
+                        where: { patientId: user[0].id },
+                        default: {
+                            statusId: "S1",
+                            doctorId: data.doctorId,
+                            patientId: user[0].id,
+                            date: data.date,
+                            timeType: data.timeType,
+                        },
+                    });
+                }
+                resolve({
+                    errCode: 0,
+                    errMessage: "Save infor patient succeed",
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+module.exports = {
+    postBookAppointment,
+};
